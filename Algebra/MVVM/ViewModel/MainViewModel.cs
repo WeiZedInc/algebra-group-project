8dc8e36eba43a8a526aac61375a31d22cdb6f371
Yurii <50027889+WeiZedInc@users.noreply.github.com>
2023-02-03 01:21:45 +0200

    Added switching between pages, basic ui ready (type 1)

--- Algebra/MVVM/ViewModel/MainViewModel.cs
+++ Algebra/MVVM/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Algebra.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,41 @@ using System.Threading.Tasks;
 
 namespace Algebra.MVVM.ViewModel
 {
-    internal class MainViewModel
+    internal class MainViewModel : ObservableObject
     {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand InformationViewCommand { get; set; }
+
+
+        public HomeViewModel HomeVM { get; set; }
+        public InformationViewModel InformationVM { get; set; }
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new();
+            InformationVM = new();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new(o =>
+            {
+                CurrentView= HomeVM;
+            });
+
+            InformationViewCommand = new(o =>
+            {
+                CurrentView = InformationVM;
+            });
+        }
     }
 }
