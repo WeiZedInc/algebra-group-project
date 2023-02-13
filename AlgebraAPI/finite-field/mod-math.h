de6a2124bc1f20d57e2239c6d767ee26a636fb60
hoksly <realhoksly@gmail.com>
2023-02-13 13:19:40 +0200

    refactored, sum prototypes added

--- AlgebraAPI/finite-field/mod-math.h
+++ AlgebraAPI/finite-field/mod-math.h
@@ -1,26 +1,50 @@
 #include "mod-int.h"
+
 #include <vector>
+#include <string>
 
 namespace modular
 {
-    template <typename _T>
-    modInt<_T> log(modInt<_T> __value, modInt<_T> __base); // discrete logarithm
+    __int128_t add(__int128_t value1, __int128_t value2, __int128_t MOD); // addition
 
-    template <typename _T>
-    modInt<_T> pow(modInt<_T> __value, modInt<_T> __base); // regular pow
+    __int128_t subs(__int128_t value1, __int128_t value2, __int128_t MOD); // substruction
 
-    template <typename _T>
-    modInt<_T> fpow(modInt<_T> __value, modInt<_T> __base); // fast pow
+    __int128_t mult(__int128_t value1, __int128_t value2, __int128_t MOD); // multyplication
 
-    template <typename _T>
-    std::vector<_T> factorize(modInt<_T> __value); // number factorization using Polard algorithm
+    ////////////////////////////////////////////////////////////////////////////////////////////
 
-    template <typename _T>
-    std::vector<_T> naiveFactorize(modInt<_T> __value); // naive number factorization
+    __int128_t div(__int128_t value1, __int128_t value2, __int128_t MOD); // division
 
-    template <typename _T>
-    modInt<_T> fpow(modInt<_T> __value, modInt<_T> __base);
+    __int128_t inv(__int128_t value1, __int128_t MOD); // inverse element
 
-    template <typename _T>
-    modInt<_T> fpow(modInt<_T> __value, modInt<_T> __base);
+    /////////////////////////////////////////////////////////////////////////////////////////////
+
+    __int128_t pow(__int128_t value, __int128_t base, __int128_t MOD); // regular pow
+
+    __int128_t fpow(__int128_t value, __int128_t base, __int128_t MOD); // fast pow
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    std::vector<__int128_t> factorize(__int128_t value, __int128_t MOD); // number factorization using Polard algorithm
+
+    std::vector<__int128_t> naiveFactorize(__int128_t value, __int128_t MOD); // naive number factorization
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    __int128_t log(__int128_t value, __int128_t base, __int128_t MOD); // discrete logarithm
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    // needs clarifications
+    bool isGenrator(__int128_t value, std::string operation, __int128_t MOD); // is element a gromGenerator
+
+    __int128_t orderOfElement(__int128_t value, __int128_t base, __int128_t MOD);
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    __int128_t eulerFunction(__int128_t value, __int128_t MOD);      // Euler function
+    __int128_t carmichaelFunction(__int128_t value, __int128_t MOD); // Carmichael function
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    bool isPrime(__int128_t value, __int128_t MOD); // Miller–Rabin primality test
 }
