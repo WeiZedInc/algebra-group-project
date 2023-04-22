9ffd83587e07cbc83700e9d873b6fd399a792291
hoksly <realhoksly@gmail.com>
2023-04-22 16:00:06 +0300

    Bug fix

--- AlgebraAPI/finite-field/source/inverse-and-division.h
+++ AlgebraAPI/finite-field/source/inverse-and-division.h
@@ -1,61 +1,66 @@
 // Function for extended Euclidean Algorithm
-template <typename num>
-num gcdExtended(num a, num b, num *x, num *y)
+#include <stdexcept>
+
+namespace modular
 {
-    // Base Case
-    if (a == 0)
+    template <typename num>
+    num gcdExtended(num a, num b, num *x, num *y)
     {
-        *x = 0;
-        *y = 1;
-        return b;
+        // Base Case
+        if (a == 0)
+        {
+            *x = 0;
+            *y = 1;
+            return b;
+        }
+        num x1, y1; // To store results of recursive call
+        num gcd = gcdExtended(b % a, a, &x1, &y1);
+        // Update x and y using results of
+        // recursive call
+        *x = y1 - (b / a) * x1;
+        *y = x1;
+        return gcd;
     }
-    num x1, y1; // To store results of recursive call
-    num gcd = gcdExtended(b % a, a, &x1, &y1);
-    // Update x and y using results of
-    // recursive call
-    *x = y1 - (b / a) * x1;
-    *y = x1;
-    return gcd;
-}
 
-/**
- *  @brief Inverses the given element in a modular field
- *  @param  value1 element to be inversed
- *  @param MOD  module
- *
- *  @return returns inversed element
- */
+    /**
+     *  @brief Inverses the given element in a modular field
+     *  @param  value1 element to be inversed
+     *  @param MOD  module
+     *
+     *  @return returns inversed element
+     */
 
-template <typename num, typename T1, typename T2, typename T3>
-T1 inv(T1 value1, T1 MOD)
-{
-    num x, y;
-
-    num g = gcdExtended(value1, MOD, x, y);
-    if (g != 1)
+    template <typename num>
+    num inv(num value1, num MOD)
     {
-        return null;
+        num x, y;
+
+        num g = gcdExtended(value1, MOD, &x, &y);
+        if (g != 1)
+        {
+            throw std::invalid_argument("Write something here");
+        }
+        else
+        {
+            x = (x % MOD + MOD) % MOD;
+            return x;
+        }
     }
-    else
+
+    /**
+     *  @brief divides value1 by value2 in a modular field
+     *  @param  value1 numerator
+     *  @param  value2 denominator
+     *  @param MOD  module
+     *
+     *
+     *  complexity O(log(n))
+     *  @return returns result of division
+     */
+
+    template <typename num>
+    num div(num value1, num value2, num MOD)
     {
-        x = (x % m + m) % m;
-        return x;
+        return value1 * inv(value2, MOD);
     }
-}
-
-/**
- *  @brief divides value1 by value2 in a modular field
- *  @param  value1 numerator
- *  @param  value2 denominator
- *  @param MOD  module
- *
- *
- *  complexity O(log(n))
- *  @return returns result of division
- */
-
-template <typename num, typename T1, typename T2, typename T3, typename T4>
-T1 div(T2 value1, T3 value2, T4 MOD)
-{
-    return value1 * inv(value2, MOD);
 }
