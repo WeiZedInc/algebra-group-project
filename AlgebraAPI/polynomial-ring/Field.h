e91c8e94aa70cef0bf34b04ec533d2d056c349e4
LonelyTrouvere <110242320+LonelyTrouvere@users.noreply.github.com>
2023-04-25 23:13:25 +0300

    Operators
    For Node class, comparison operators fixed and + - operators added

--- AlgebraAPI/polynomial-ring/Field.h
+++ AlgebraAPI/polynomial-ring/Field.h
@@ -1,32 +1,86 @@
 #pragma once
 #include <vector>
+#include <iostream>
 
 template <typename T>
 class Node {
-private: 
-	T pow;
+private:
+	size_t pow;
 	T koef;
 
-public: 
+public:
+	Node() {
+		pow = 0;
+		koef = 0;
+	}
 	Node(T p1, T k1) {
 		pow = p1; koef = k1;
 	}
-	~Node();
+	~Node() {}
 
-	T power();
+	size_t power();
 	T koeficient();
 
-	bool operator > (const Node p1, const Node p2);
-	bool operator >= (const Node p1, const Node p2);
-	bool operator < (const Node p1, const Node p2);
-	bool operator <= (const Node p1, const Node p2);
-	bool operator == (const Node p1, const Node p2);
-	bool operator != (const Node p1, const Node p2);
+	bool operator>(const Node &p2) {
+		return pow > p2.pow;
+	}
+
+	bool operator>=(const Node p2) {
+		return pow >= p2.pow;
+	}
+
+	bool operator < (const Node p2) {
+		return pow < p2.pow;
+	}
+
+	bool operator <= (const Node p2) {
+		return pow <= p2.pow;
+	}
+
+	bool operator == (const Node p2) {
+		return pow == p2.pow;
+	}
+
+	bool operator != (const Node p2) {
+		return pow != p2.pow;
+	}
+
+	Node operator + (const Node p2) {
+		try {
+			if (pow != p2.pow)
+				throw "Can't add monominals with different powers";
+			else {
+				Node res(pow, koef + p2.koef);
+				return res;
+			}
+		}
+		catch (const char* e)
+		{
+			std::cerr << e;
+			exit(69);
+		}
+	}
+
+	Node operator - (const Node p2) {
+		try {
+			if (pow != p2.pow)
+				throw "Can't substract monominals with different powers";
+			else {
+				Node res(pow, koef - p2.koef);
+				return res;
+			}
+		}
+		catch (const char* e)
+		{
+			std::cerr << e;
+			exit(69);
+		}
+	}
 };
 
 template <typename T>
 class Field {
-private: 
+private:
 	std::vector<Node<T>> poly;
 
 public:
@@ -34,10 +88,11 @@ public:
 	~Field();
 
 	Node<T> operator [] (int i);
+	void addNode();
 };
 
 template <typename T>
-T Node<T>::power() {
+size_t Node<T>::power() {
 	return pow;
 }
 
@@ -47,53 +102,8 @@ T Node<T>::koeficient() {
 }
 
 template <typename T>
-bool Node<T>::operator > (const Node<T> p1, const Node<T> p2) {
-	if (p1.pow > p2.pow) return true;
-	else
-		return false;
-}
-
-template <typename T>
-bool Node<T>::operator >= (const Node<T> p1, const Node<T> p2) {
-	if (p1.pow >= p2.pow) return true;
-	else
-		return false;
-}
-
-
-template <typename T>
-bool Node<T>::operator < (const Node<T> p1, const Node<T> p2) {
-	if (p1.pow < p2.pow) return true;
-	else
-		return false;
-}
-
-
-
-template <typename T>
-bool Node<T>::operator <= (const Node<T> p1, const Node<T> p2) {
-	if (p1.pow <= p2.pow) return true;
-	else
-		return false;
-}
-
-template <typename T>
-bool Node<T>::operator == (const Node<T> p1, const Node<T> p2) {
-	if (p1.pow == p2.pow) return true;
-	else
-		return false;
-}
-
-template <typename T>
-bool Node<T>::operator != (const Node<T> p1, const Node<T> p2) {
-	if (p1.pow != p2.pow) return true;
-	else
-		return false;
-}
-
-
-template <typename T>
 Node<T> Field<T>::operator [] (int i) {
 	return poly[i];
 }
+
 
