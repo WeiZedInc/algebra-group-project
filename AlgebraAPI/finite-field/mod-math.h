7e02e43fba1c9ccffa0a944517118fdd4d3eef63
hoksly <realhoksly@gmail.com>
2023-04-28 10:10:40 +0300

    Changed types to modNum, typo and style correction

--- AlgebraAPI/finite-field/mod-math.h
+++ AlgebraAPI/finite-field/mod-math.h
@@ -1,78 +1,79 @@
-#include <vector>
 #include <string>
+#include <vector>
 
-namespace modular
-{
+namespace modular {
 #ifndef MOD_NUM
 #define MOD_NUM
-   template <typename T>
-   class modNum
-   {
+template <typename T>
+class modNum {
    private:
-      mutable T value, MOD;
+    mutable T value, MOD;
 
-      T add(T value1, T value2, T MOD) const;
-      T subs(T value1, T value2, T MOD) const;
-      T mult(T value1, T value2, T MOD) const;
-      T gcdExtended(T a, T b, T *x, T *y) const;
-      T inverseValue(T value1, T mod) const;
-      T div(T value1, T value2, T mod) const;
+    T add(T value1, T value2, T MOD) const;
+    T subs(T value1, T value2, T MOD) const;
+    T mult(T value1, T value2, T MOD) const;
+    T gcdExtended(T a, T b, T *x, T *y) const;
+    T inverseValue(T value1, T mod) const;
+    T div(T value1, T value2, T mod) const;
 
    public:
-      T getValue() const;
-      void setMod(T MOD);
-      modNum(T value = 0, T MOD = 1) : value(value), MOD(MOD)
-      {
-
-         if (MOD <= 0)
-         {
+    T getValue() const;
+    T getMod() const { return MOD; };
+    void setMod(T MOD);
+    modNum(T value = 0, T MOD = 1) : value(value), MOD(MOD) {
+        if (MOD <= 0) {
             throw std::invalid_argument("modulus should be positive");
-         }
-      }
+        }
+    }
 
-      modNum inv();
+    modNum inv();
 
-      modNum<T> operator+(const modNum &other) const;
-      modNum<T> operator-(const modNum &other) const;
-      modNum<T> operator*(const modNum &other) const;
-      modNum<T> operator/(const modNum &other) const;
-   };
+    modNum<T> operator+(const modNum &other) const;
+    modNum<T> operator-(const modNum &other) const;
+    modNum<T> operator*(const modNum &other) const;
+    modNum<T> operator/(const modNum &other) const;
+};
 #endif
 
-   /////////////////////////////////////////////////////////////////////////////////////////////
-   template <typename T1>
-   T1 pow(T1 value, T1 base, T1 MOD); // regular pow
-   template <typename T1>
-   T1 fpow(T1 value, T1 base, T1 MOD); // fast pow
+/////////////////////////////////////////////////////////////////////////////////////////////
+template <typename T1>
+modNum<T1> pow(modNum<T1> value, modNum<T1> base,
+               modNum<T1> MOD);   // regular pow
+template <typename T1>
+modNum<T1> fpow(modNum<T1> value, modNum<T1> base,
+                modNum<T1> MOD);   // fast pow
 
-   //////////////////////////////////////////////////////////////////////////////////////////////
-   template <typename T1>
-   std::vector<T1> factorize(T1 value, T1 MOD); // number factorization using Polard algorithm
+//////////////////////////////////////////////////////////////////////////////////////////////
+template <typename T1>
+std::vector<modNum<T1>> factorize(
+    modNum<T1> value);   // number factorization using Polard algorithm
 
-   //////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
 
-   template <typename T1>
-   T1 log(T1 value, T1 base, T1 MOD); // discrete logarithm
+template <typename T1>
+modNum<T1> log(modNum<T1> value, modNum<T1> base);   // discrete logarithm
 
-   //////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
 
-   // needs clarifications
-   template <typename T1>
-   bool isGenrator(T1 value, std::string operation, T1 MOD); // is element a gromGenerator
+// needs clarifications
+template <typename T1>
+bool isGenrator(modNum<T1> value,
+                std::string operation);   // is element a group generator
 
-   template <typename T1>
-   T1 orderOfElement(T1 value, T1 base, T1 MOD);
+template <typename T1>
+size_t orderOfElement(modNum<T1> value, modNum<T1> base);
 
-   //////////////////////////////////////////////////////////////////////////////////////////////
-   template <typename T1>
-   T1 eulerFunction(T1 value, T1 MOD); // Euler function
+//////////////////////////////////////////////////////////////////////////////////////////////
+template <typename T1>
+modNum<T1> eulerFunction(modNum<T1> value);   // Euler function
 
-   template <typename T1>
-   T1 carmichaelFunction(T1 value, T1 MOD); // Carmichael function
+template <typename T1>
+modNum<T1> carmichaelFunction(modNum<T1> value);   // Carmichael function
 
-   //////////////////////////////////////////////////////////////////////////////////////////////
-   template <typename T1>
-   bool isPrime(T1 value, T1 k); // Miller–Rabin primality test
-}
+//////////////////////////////////////////////////////////////////////////////////////////////
+template <typename T1>
+bool isPrime(modNum<T1> value, size_t k);   // Miller–Rabin primality test
+
+}   // namespace modular
 
 #include "source/mod-num.tcc"
