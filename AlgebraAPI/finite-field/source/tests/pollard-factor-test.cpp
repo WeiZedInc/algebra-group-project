58bcbe3da1ad2ebed3373f774f6671f3bf344e1a
hoksly <realhoksly@gmail.com>
2023-04-29 12:04:06 +0300

    polard factorization bug fix

--- AlgebraAPI/finite-field/source/tests/pollard-factor-test.cpp
+++ AlgebraAPI/finite-field/source/tests/pollard-factor-test.cpp
@@ -1,59 +1,56 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "../../../doctest.h"
 #include "../pollard-factor.tcc"
-#include "utils.h"
+
 #include <random>
 
+#include "../../../doctest.h"
+#include "utils.h"
 
-TEST_CASE("Testing Pollard factorization")
-{
+TEST_CASE("Testing Pollard factorization") {
     using T = long long;
-    SUBCASE("Test1")
-    {
-       std::vector<modNum<T>> f1 = factorize(modNum<T>(10403));
-       std::vector<modNum<T>> factors1{modNum<T>(101), modNum<T>(103)};
-       REQUIRE(f1 == factors1);
+    SUBCASE("Test1") {
+        std::vector<modNum<T>> f1 = factorize(modNum<T>(10403));
+        std::vector<modNum<T>> factors1{modNum<T>(101), modNum<T>(103)};
+        sort(f1.begin(), f1.end());
+        REQUIRE(f1 == factors1);
 
-       std::vector<modNum<T>> f2 = factorize(modNum<T>(121));
-       std::vector<modNum<T>> factors2{modNum<T>(11), modNum<T>(11)};
-       REQUIRE(f2 == factors2);
+        std::vector<modNum<T>> f2 = factorize(modNum<T>(121));
+        std::vector<modNum<T>> factors2{modNum<T>(11), modNum<T>(11)};
+        sort(factors1.begin(), factors1.end());
+        REQUIRE(f2 == factors2);
 
-       std::vector<modNum<T>> f3 = factorize(modNum<T>(433));
-       std::vector<modNum<T>> factors3{modNum<T>(433)};
-       REQUIRE(f3 == factors3);
+        std::vector<modNum<T>> f3 = factorize(modNum<T>(433));
+        std::vector<modNum<T>> factors3{modNum<T>(433)};
+        REQUIRE(f3 == factors3);
 
-       std::vector<modNum<T>> f4 = factorize(modNum<T>(1232));
-       std::vector<modNum<T>> factors4{modNum<T>(2), modNum<T>(2), modNum<T>(2), modNum<T>(2), modNum<T>(7), modNum<T>(11)};
-       REQUIRE(f4 == factors4);
+        std::vector<modNum<T>> f4 = factorize(modNum<T>(1232));
+        sort(f4.begin(), f4.end());
+        std::vector<modNum<T>> factors4{modNum<T>(2), modNum<T>(2),
+                                        modNum<T>(2), modNum<T>(2),
+                                        modNum<T>(7), modNum<T>(11)};
+        REQUIRE(f4 == factors4);
     }
 
-    SUBCASE("Test2")
-    {
-        for (long long i = 2; i <= 100000; ++i) 
-        {
+    SUBCASE("Test2") {
+        for (long long i = 2; i <= 100000; ++i) {
             bool isExceptionThrown = false;
-            try 
-            {
+            try {
                 std::vector<modNum<T>> num1 = factorize(modNum<T>(i));
-            }
-            catch (std::exception &e)
-            {
+            } catch (std::exception &e) {
                 isExceptionThrown = true;
             }
             REQUIRE(!isExceptionThrown);
         }
     }
 
-    SUBCASE("Test3")
-    {
+    SUBCASE("Test3") {
         modNum<T> a(getRandomNumber(1, getRandomNumber(1, 100000)));
 
         std::vector<modNum<T>> f1 = factorize(a);
 
         T mult = 1;
         bool isExceptionThrown = false;
-        for (auto numb : f1)
-        {
+        for (auto numb : f1) {
             if (!isPrime(numb.getValue()))
                 break;
             mult *= numb.getValue();
@@ -61,6 +58,4 @@ TEST_CASE("Testing Pollard factorization")
 
         REQUIRE(a.getValue() == mult);
     }
-
-   
 }
