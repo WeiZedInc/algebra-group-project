89a408411757d4cbcc530eb54fbde49f4e81c6cf
hoksly <realhoksly@gmail.com>
2023-05-05 19:50:59 +0300

    synchronized with tcc files

--- AlgebraAPI/polynomial-ring/poly-ring-math.h
+++ AlgebraAPI/polynomial-ring/poly-ring-math.h
@@ -17,24 +17,24 @@ private:
 
 public:
     Node();
-    Node(T koef, size_t degree);
+    Node(modNum<T> koef, size_t degree);
     ~Node() = default;
 
     size_t deg() const;
     modNum<T> k() const;
 
-    bool operator>(const Node<modNum<T>> &p2) const;
-    bool operator>=(const Node<modNum<T>> &p2) const;
-    bool operator<(const Node<modNum<T>> &p2) const;
-    bool operator<=(const Node<modNum<T>> &p2) const;
+    bool operator>(const Node<T> &p2) const;
+    bool operator>=(const Node<T> &p2) const;
+    bool operator<(const Node<T> &p2) const;
+    bool operator<=(const Node<T> &p2) const;
 
-    bool operator!=(const Node<modNum<T>> &p2) const;
-    Node<modNum<T>> operator+(const Node<modNum<T>> &p2) const;
-    Node<modNum<T>> operator-(const Node<modNum<T>> &p2) const;
-    void operator=(const Node<modNum<T>> &p2);
-    bool operator==(const Node<modNum<T>> &p2) const;
+    bool operator!=(const Node<T> &p2) const;
+    Node<T> operator+(const Node<T> &p2) const;
+    Node<T> operator-(const Node<T> &p2) const;
+    void operator=(const Node<T> &p2);
+    bool operator==(const Node<T> &p2) const;
 
-    modNum<T> evaluate() const { return fpow(koef, degree); };
+    modNum<T> evaluate(T) const;
 };
 #endif
 
@@ -44,48 +44,55 @@ template <typename T>
 class Polynomial
 {
 private:
-    std::list<Node<modNum<T>>> poly;
-    size_t degree; // polynomial degree
-    T numMod;
+    std::list<Node<T>> poly;
+    size_t degree = 0;
+    T numMod = 0;
 
 public:
-    Polynomial() = default;
+    Polynomial(T mod) : numMod(mod){};
+    Polynomial(std::vector<std::pair<T, size_t>>, T);
+    Polynomial(std::pair<T, size_t> *arr, size_t n, T);
 
     ~Polynomial() = default;
 
     Node<T> operator[](const size_t i); // use only when really necessary
 
     bool empty();
-    typename std::list<Node<modNum<T>>>::const_iterator begin() const { return poly.begin(); };
-    typename std::list<Node<modNum<T>>>::const_iterator end() const { return poly.end(); };
+    typename std::list<Node<T>>::const_iterator begin() const { return poly.begin(); };
+    typename std::list<Node<T>>::const_iterator end() const { return poly.end(); };
 
-    void addNode(const Node<modNum<T>>);
+    void addNode(const Node<T>);
+    void addNode(const T, size_t);
 
-    void removeNode(const Node<modNum<T>>); // by value
-    void removeNode(const size_t);          // by degree
+    void removeNode(const Node<T>); // by value
+    void removeNode(const size_t);  // by degree
 
-    size_t deg() const { return degree; };
+    size_t getDegree() const { return degree; };
+    T getNumMod() const { return numMod; }
 
-    //////////////////////////////////////////////////////////////////////////////
-
-    Polynomial<modNum<T>> operator+(const Polynomial<modNum<T>> &) const;
-    Polynomial<modNum<T>> operator-(const Polynomial<modNum<T>> &) const;
-    Polynomial<modNum<T>> operator*(const Polynomial<modNum<T>> &) const;
+    void print() const;
 
     //////////////////////////////////////////////////////////////////////////////
 
-    Polynomial<modNum<T>> der() const; // deriveative
-    modNum<modNum<T>> evaluate() const;
+    Polynomial<T> operator+(const Polynomial<T> &) const;
+    Polynomial<T> operator-(const Polynomial<T> &) const;
+    Polynomial<T> operator*(const Polynomial<T> &) const;
 
     //////////////////////////////////////////////////////////////////////////////
 
-    std::pair<Polynomial<modNum<T>>, Polynomial<modNum<T>>> operator/(const Polynomial<T> &) const;
-
-    Polynomial<modNum<T>> gcd(const Polynomial<modNum<T>> &) const;
+    Polynomial<T> der() const; // deriveative
+    modNum<T> evaluate(const T) const;
+    modNum<T> evaluate(const modNum<T>) const;
 
     //////////////////////////////////////////////////////////////////////////////
 
-    static Polynomial<modNum<T>> getPolynomialByOrder(size_t);
+    std::pair<Polynomial<T>, Polynomial<T>> operator/(const Polynomial<T> &) const;
+
+    Polynomial<T> gcd(const Polynomial<T> &) const;
+
+    //////////////////////////////////////////////////////////////////////////////
+
+    static Polynomial<T> getPolynomialByOrder(size_t);
 };
 #endif
 
