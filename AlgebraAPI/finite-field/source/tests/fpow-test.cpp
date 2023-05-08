6a7402ed4a23b7a4d00e792d5391df99085dadd1
hoksly <realhoksly@gmail.com>
2023-05-08 22:46:59 +0300

    save changes commit

--- AlgebraAPI/finite-field/source/tests/fpow-test.cpp
+++ AlgebraAPI/finite-field/source/tests/fpow-test.cpp
@@ -1,3 +1,4 @@
+
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "../fpow.tcc"
 
@@ -6,9 +7,9 @@
 #include "../../../doctest.h"
 #include "utils.h"
 using namespace modular;
+
 template <class T>
-T
-logPow(T base, T power, T MOD) {
+T logPow(T base, T power, T MOD) {
     T result = 1;
     while (power > 0) {
         if (power % 2 == 1) {   // Can also use (power & 1) to make code even faster
@@ -20,13 +21,21 @@ logPow(T base, T power, T MOD) {
     return result;
 }
 
-TEST_CASE("randomized test 1..1000 with mod 13") {
+/*TEST_CASE("randomized test 1..1000 with mod 13") {
     for (int i = 0; i < 1000; ++i) {
         int num = getRandomNumber(1, 1000);
         int pw = getRandomNumber(1, 1000);
 
         // CHECK_EQ(fpow(modNum(num, 47), pw).getValue(), logPow(num, pw, 47));
     }
-}
+}*/
 
-TEST_CASE("randomized test 1..1000 with mod 7") {}
+TEST_CASE("fpow - random values") {
+    modNum<int> a = getRandomNumber(1,1000);
+    int b = getRandomNumber(1,1000);
+
+    for(int i = 1; i <= 100; i++){
+        CHECK_EQ(fpow(a, b),logPow(a,b,a.getMod()));
+    }
+
+}
