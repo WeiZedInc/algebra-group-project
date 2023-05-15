1cb4013b0e282f9ca95b5b6e4c9cadd36f4b2279
tassssia <113410135+tassssia@users.noreply.github.com>
2023-05-15 18:10:47 +0300

    Montgomery reduction added

--- AlgebraAPI/finite-field/source/tests/fpow-test.cpp
+++ AlgebraAPI/finite-field/source/tests/fpow-test.cpp
@@ -6,25 +6,12 @@
 
 using namespace modular;
 
-template <class T>
-T
-logPow(T base, T power, T MOD) {
-    T result = 1;
-    while (power > 0) {
-        if (power % 2 == 1) {   // Can also use (power & 1) to make code even faster
-            result = (result * base) % MOD;
-        }
-        base = (base * base) % MOD;
-        power = power / 2;   // Can also use power >>= 1; to make code even faster
-    }
-    return result;
-}
 
-/*TEST_CASE("randomized test 1..1000 with mod 13") {
+TEST_CASE("randomized test 1..1000 with mod 13") {
     for (int i = 0; i < 1000; ++i) {
-        int num = getRandomNumber(1, 1000);
+        int num = getRandomNumber(1, 10000000);
         int pw = getRandomNumber(1, 1000);
 
-        // CHECK_EQ(fpow(modNum(num, 47), pw).getValue(), logPow(num, pw, 47));
+        CHECK_EQ(fpow(modNum(num, 47), pw).getValue(), logPow(num, pw, 47));
     }
-}*/
+}
