2150685d65f6d8ed8929aebe5cb794f41875f0c1
rsukhaniuk <rsukhaniuk@gmail.com>
2023-05-17 01:03:37 +0300

    numerator degree < divisor degree test-case

--- AlgebraAPI/polynomial-ring/source/tests/poly-basic-test.cpp
+++ AlgebraAPI/polynomial-ring/source/tests/poly-basic-test.cpp
@@ -5,8 +5,10 @@
 
 using namespace modular;
 
-TEST_CASE("Testing") {
-    SUBCASE("Test addition 1") {
+TEST_CASE("Testing")
+{
+    SUBCASE("Test addition 1")
+    {
         Polynomial<mpz_class> p1(5);
         p1.addNode(2, 0);
         p1.addNode(3, 1);
@@ -27,7 +29,8 @@ TEST_CASE("Testing") {
         REQUIRE(p3 == (p1 + p2));
     }
 
-    SUBCASE("Test addition 2") {
+    SUBCASE("Test addition 2")
+    {
         Polynomial<mpz_class> p1(5);
         p1.addNode(2, 0);
         p1.addNode(3, 1);
@@ -45,7 +48,8 @@ TEST_CASE("Testing") {
         REQUIRE(p3 == (p1 + p2));
     }
 
-    SUBCASE("Test addition 3") {
+    SUBCASE("Test addition 3")
+    {
         Polynomial<mpz_class> p1(7);
         p1.addNode(4, 0);
         p1.addNode(2, 1);
@@ -64,7 +68,8 @@ TEST_CASE("Testing") {
         REQUIRE(p3 == (p1 + p2));
     }
 
-    SUBCASE("Test subtraction 1") {
+    SUBCASE("Test subtraction 1")
+    {
         Polynomial<mpz_class> p1(5);
         p1.addNode(2, 0);
         p1.addNode(3, 1);
@@ -83,7 +88,8 @@ TEST_CASE("Testing") {
         REQUIRE(p3 == (p1 - p2));
     }
 
-    SUBCASE("Test subtraction 2") {
+    SUBCASE("Test subtraction 2")
+    {
         Polynomial<mpz_class> p1(5);
         p1.addNode(3, 0);
         p1.addNode(5, 1);
@@ -101,7 +107,8 @@ TEST_CASE("Testing") {
         REQUIRE(p3 == (p1 - p2));
     }
 
-    SUBCASE("Test subtraction 3") {
+    SUBCASE("Test subtraction 3")
+    {
         Polynomial<mpz_class> p1(2);
         p1.addNode(3, 0);
         p1.addNode(7, 1);
@@ -121,7 +128,8 @@ TEST_CASE("Testing") {
         REQUIRE(p3 == (p1 - p2));
     }
 
-    SUBCASE("Test multiplication 1") {
+    SUBCASE("Test multiplication 1")
+    {
         Polynomial<mpz_class> p1(5);
         p1.addNode(3, 5);
         p1.addNode(7, 3);
@@ -144,7 +152,8 @@ TEST_CASE("Testing") {
         REQUIRE(p3 == (p1 * p2));
     }
 
-    SUBCASE("Test multiplication 2") {
+    SUBCASE("Test multiplication 2")
+    {
         Polynomial<mpz_class> p1(5);
         p1.addNode(2, 0);
         p1.addNode(3, 1);
@@ -164,7 +173,8 @@ TEST_CASE("Testing") {
         REQUIRE(p3 == (p1 * p2));
     }
 
-    SUBCASE("Test multiplication 3") {
+    SUBCASE("Test multiplication 3")
+    {
         Polynomial<mpz_class> p1(2);
         p1.addNode(1, 0);
         p1.addNode(1, 1);
@@ -180,7 +190,8 @@ TEST_CASE("Testing") {
         REQUIRE(p3 == (p1 * p2));
     }
 
-    SUBCASE("Test multiplication 4") {
+    SUBCASE("Test multiplication 4")
+    {
         Polynomial<mpz_class> p1(2);
         p1.addNode(1, 0);
         p1.addNode(3, 1);
@@ -195,8 +206,10 @@ TEST_CASE("Testing") {
         REQUIRE(p3 == (p1 * p2));
     }
 }
-TEST_CASE("Testing division, gcd") {
-    SUBCASE("Test 0 - remainder") {
+TEST_CASE("Testing division, gcd")
+{
+    SUBCASE("Test 0 - remainder")
+    {
         Polynomial<mpz_class> p1(7);
         p1.addNode(1, 2);
         p1.addNode(1, 1);
@@ -211,15 +224,16 @@ TEST_CASE("Testing division, gcd") {
 
         Polynomial<mpz_class> res = p1 % p2;
 
-        //res.print();
+        // res.print();
 
         Polynomial<mpz_class> expected(2);
         expected.addNode(1, 0);
 
         REQUIRE(res == expected);
     }
-    
-    SUBCASE("Test 1") {
+
+    SUBCASE("Test 1")
+    {
         Polynomial<mpz_class> p1(37);
         p1.addNode(2, 4);
         p1.addNode(27, 3);
@@ -238,7 +252,8 @@ TEST_CASE("Testing division, gcd") {
         REQUIRE(p1 == test);
     }
 
-    SUBCASE("Test 2") {
+    SUBCASE("Test 2")
+    {
         Polynomial<mpz_class> p1(5);
         p1.addNode(3, 4);
         p1.addNode(1, 3);
@@ -256,7 +271,8 @@ TEST_CASE("Testing division, gcd") {
         REQUIRE(p1 == test);
     }
 
-    SUBCASE("Test 3 - divisor - constant") {
+    SUBCASE("Test 3 - divisor - constant")
+    {
         Polynomial<mpz_class> p1(37);
         p1.addNode(6, 4);
         p1.addNode(27, 3);
@@ -274,7 +290,8 @@ TEST_CASE("Testing division, gcd") {
         REQUIRE(p1 == test);
     }
 
-    SUBCASE("Test 4 - p2 empty") {
+    SUBCASE("Test 4 - p2 empty")
+    {
         Polynomial<mpz_class> p1(37);
         p1.addNode(6, 4);
         p1.addNode(27, 3);
@@ -287,22 +304,26 @@ TEST_CASE("Testing division, gcd") {
         CHECK_THROWS_WITH(p1 / p2, "Divisor must have at least one non-zero coefficient");
     }
 
-    SUBCASE("Test 5 - numerator degree < divisor degree") {
-        Polynomial<mpz_class> p1(37);
-        p1.addNode(2, 4);
-        p1.addNode(27, 3);
-        p1.addNode(36, 2);
+    SUBCASE("Test 5 - numerator degree < divisor degree")
+    {
+        Polynomial<mpz_class> p1(11);
+        p1.addNode(6, 3);
+        p1.addNode(8, 2);
         p1.addNode(10, 1);
-        p1.addNode(27, 0);
+        p1.addNode(1, 0);
 
-        Polynomial<mpz_class> p2(37);
-        p2.addNode(2, 1);
-        p2.addNode(3, 0);
+        Polynomial<mpz_class> p2(11);
+        p2.addNode(7, 7);
 
-        CHECK_THROWS_WITH(p2 / p1, "The degree of the divisor cannot exceed that of the numerator");
+        std::pair<Polynomial<mpz_class>, Polynomial<mpz_class>> res = p1.divClassic(p2);
+
+        Polynomial<mpz_class> test = res.first * p2 + res.second;
+
+        // CHECK_THROWS_WITH(p2 / p1, "The degree of the divisor cannot exceed that of the numerator");
     }
 
-    SUBCASE("Test 6 - gcd") {
+    SUBCASE("Test 6 - gcd")
+    {
         Polynomial<mpz_class> p1(2);
         p1.addNode(1, 2);
         p1.addNode(1, 0);
@@ -320,7 +341,8 @@ TEST_CASE("Testing division, gcd") {
         REQUIRE(gcd == expected);
     }
 
-    SUBCASE("Test 7 - gcd") {
+    SUBCASE("Test 7 - gcd")
+    {
         Polynomial<mpz_class> p1(5);
         p1.addNode(2, 4);
         p1.addNode(3, 3);
@@ -340,7 +362,8 @@ TEST_CASE("Testing division, gcd") {
         REQUIRE(gcd == expected);
     }
 
-    SUBCASE("Test 8 - gcd") {
+    SUBCASE("Test 8 - gcd")
+    {
         Polynomial<mpz_class> p1(7);
         p1.addNode(3, 4);
         p1.addNode(6, 3);
@@ -361,7 +384,8 @@ TEST_CASE("Testing division, gcd") {
         REQUIRE(gcd == expected);
     }
 
-    SUBCASE("Test 9 - gcd") {
+    SUBCASE("Test 9 - gcd")
+    {
         Polynomial<mpz_class> p1(3);
         p1.addNode(1, 4);
         p1.addNode(2, 3);
@@ -380,7 +404,8 @@ TEST_CASE("Testing division, gcd") {
 
         REQUIRE(gcd == expected);
     }
-    SUBCASE("Test 10 - gcd") {
+    SUBCASE("Test 10 - gcd")
+    {
         Polynomial<mpz_class> p1(7);
         p1.addNode(3, 6);
         p1.addNode(2, 2);
@@ -401,7 +426,8 @@ TEST_CASE("Testing division, gcd") {
         REQUIRE(gcd == expected);
     }
 
-    SUBCASE("Test 11 - remainder") {
+    SUBCASE("Test 11 - remainder")
+    {
         Polynomial<mpz_class> p1(3);
         p1.addNode(1, 80);
         p1.addNode(-1, 0);
@@ -414,14 +440,14 @@ TEST_CASE("Testing division, gcd") {
         Polynomial<mpz_class> res = p1 % p2;
 
         Polynomial<mpz_class> expected(3);
-        expected.addNode(0,0);
+        expected.addNode(0, 0);
 
         REQUIRE(res == expected);
     }
-
 }
 
-TEST_CASE("New constructor") {
+TEST_CASE("New constructor")
+{
     std::vector<std::pair<int, size_t>> p1 = {{3, 3}, {4, 2}, {5, 1}, {6, 0}};
     std::vector<std::pair<int, size_t>> p2 = {{4, 4}};
 
