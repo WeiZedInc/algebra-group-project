dee0e4288624972ecb96838d2e90073fa651273f
rsukhaniuk <rsukhaniuk@gmail.com>
2023-05-16 15:38:36 +0300

    mpz_class gcd and div tests

--- AlgebraAPI/polynomial-ring/source/tests/poly-basic-test.cpp
+++ AlgebraAPI/polynomial-ring/source/tests/poly-basic-test.cpp
@@ -1,6 +1,7 @@
 ﻿#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "../../../doctest.h"
 #include "../../poly-ring-math.h"
+#include <gmpxx.h>
 
 using namespace modular;
 
@@ -196,82 +197,82 @@ TEST_CASE("Testing") {
 }
 TEST_CASE("Testing division, gcd") {
     SUBCASE("Test 1") {
-        Polynomial<int> p1(37);
+        Polynomial<mpz_class> p1(37);
         p1.addNode(2, 4);
         p1.addNode(27, 3);
         p1.addNode(36, 2);
         p1.addNode(10, 1);
         p1.addNode(27, 0);
 
-        Polynomial<int> p2(37);
+        Polynomial<mpz_class> p2(37);
         p2.addNode(2, 1);
         p2.addNode(3, 0);
 
-        std::pair<Polynomial<int>, Polynomial<int>> res = p1.divClassic(p2);
+        std::pair<Polynomial<mpz_class>, Polynomial<mpz_class>> res = p1.divClassic(p2);
 
-        Polynomial<int> test = res.first * p2 + res.second;
+        Polynomial<mpz_class> test = res.first * p2 + res.second;
 
         REQUIRE(p1 == test);
     }
 
     SUBCASE("Test 2") {
-        Polynomial<int> p1(5);
+        Polynomial<mpz_class> p1(5);
         p1.addNode(3, 4);
         p1.addNode(1, 3);
         p1.addNode(2, 2);
         p1.addNode(1, 0);
 
-        Polynomial<int> p2(5);
+        Polynomial<mpz_class> p2(5);
         p2.addNode(1, 2);
         p2.addNode(4, 1);
         p2.addNode(2, 0);
 
-        std::pair<Polynomial<int>, Polynomial<int>> res = p1.divClassic(p2);
-        Polynomial<int> test = res.first * p2 + res.second;
+        std::pair<Polynomial<mpz_class>, Polynomial<mpz_class>> res = p1.divClassic(p2);
+        Polynomial<mpz_class> test = res.first * p2 + res.second;
 
         REQUIRE(p1 == test);
     }
 
     SUBCASE("Test 3 - divisor - constant") {
-        Polynomial<int> p1(37);
+        Polynomial<mpz_class> p1(37);
         p1.addNode(6, 4);
         p1.addNode(27, 3);
         p1.addNode(36, 2);
         p1.addNode(9, 1);
         p1.addNode(27, 0);
 
-        Polynomial<int> p2(37);
+        Polynomial<mpz_class> p2(37);
         p2.addNode(3, 0);
 
-        std::pair<Polynomial<int>, Polynomial<int>> res = p1.divClassic(p2);
+        std::pair<Polynomial<mpz_class>, Polynomial<mpz_class>> res = p1.divClassic(p2);
 
-        Polynomial<int> test = res.first * p2 + res.second;
+        Polynomial<mpz_class> test = res.first * p2 + res.second;
 
         REQUIRE(p1 == test);
     }
 
     SUBCASE("Test 4 - p2 empty") {
-        Polynomial<int> p1(37);
+        Polynomial<mpz_class> p1(37);
         p1.addNode(6, 4);
         p1.addNode(27, 3);
         p1.addNode(36, 2);
         p1.addNode(9, 1);
         p1.addNode(27, 0);
 
-        Polynomial<int> p2(37);
+        Polynomial<mpz_class> p2(37);
 
         CHECK_THROWS_WITH(p1 / p2, "Divisor must have at least one non-zero coefficient");
     }
 
     SUBCASE("Test 5 - numerator degree < divisor degree") {
-        Polynomial<int> p1(37);
+        Polynomial<mpz_class> p1(37);
         p1.addNode(2, 4);
         p1.addNode(27, 3);
         p1.addNode(36, 2);
         p1.addNode(10, 1);
         p1.addNode(27, 0);
 
-        Polynomial<int> p2(37);
+        Polynomial<mpz_class> p2(37);
         p2.addNode(2, 1);
         p2.addNode(3, 0);
 
@@ -279,17 +280,17 @@ TEST_CASE("Testing division, gcd") {
     }
 
     SUBCASE("Test 6 - gcd") {
-        Polynomial<int> p1(2);
+        Polynomial<mpz_class> p1(2);
         p1.addNode(1, 2);
         p1.addNode(1, 0);
 
-        Polynomial<int> p2(2);
+        Polynomial<mpz_class> p2(2);
         p2.addNode(1, 1);
         p2.addNode(1, 0);
 
         auto gcd = p1.gcd(p2);
 
-        Polynomial<int> expected(2);
+        Polynomial<mpz_class> expected(2);
         expected.addNode(1, 1);
         expected.addNode(1, 0);
 
@@ -297,19 +298,19 @@ TEST_CASE("Testing division, gcd") {
     }
 
     SUBCASE("Test 7 - gcd") {
-        Polynomial<int> p1(5);
+        Polynomial<mpz_class> p1(5);
         p1.addNode(2, 4);
         p1.addNode(3, 3);
         p1.addNode(3, 1);
         p1.addNode(3, 0);
 
-        Polynomial<int> p2(5);
+        Polynomial<mpz_class> p2(5);
         p2.addNode(1, 3);
         p2.addNode(4, 1);
         p2.addNode(1, 0);
 
         auto gcd = p1.gcd(p2);
-        Polynomial<int> expected(5);
+        Polynomial<mpz_class> expected(5);
         expected.addNode(1, 1);
         expected.addNode(2, 0);
 
@@ -317,20 +318,20 @@ TEST_CASE("Testing division, gcd") {
     }
 
     SUBCASE("Test 8 - gcd") {
-        Polynomial<int> p1(7);
+        Polynomial<mpz_class> p1(7);
         p1.addNode(3, 4);
         p1.addNode(6, 3);
         p1.addNode(3, 2);
         p1.addNode(2, 1);
         p1.addNode(3, 0);
 
-        Polynomial<int> p2(7);
+        Polynomial<mpz_class> p2(7);
         p2.addNode(2, 3);
         p2.addNode(5, 1);
         p2.addNode(1, 0);
 
         auto gcd = p1.gcd(p2);
-        Polynomial<int> expected(5);
+        Polynomial<mpz_class> expected(5);
         expected.addNode(1, 1);
         expected.addNode(4, 0);
 
@@ -338,32 +339,32 @@ TEST_CASE("Testing division, gcd") {
     }
 
     SUBCASE("Test 9 - gcd") {
-        Polynomial<int> p1(3);
+        Polynomial<mpz_class> p1(3);
         p1.addNode(1, 4);
         p1.addNode(2, 3);
         p1.addNode(2, 1);
         p1.addNode(1, 0);
 
-        Polynomial<int> p2(3);
+        Polynomial<mpz_class> p2(3);
         p2.addNode(1, 3);
         p2.addNode(2, 2);
         p2.addNode(1, 0);
 
         auto gcd = p1.gcd(p2);
 
-        Polynomial<int> expected(3);
+        Polynomial<mpz_class> expected(3);
         expected.addNode(1, 0);
 
         REQUIRE(gcd == expected);
     }
     SUBCASE("Test 10 - gcd") {
-        Polynomial<int> p1(7);
+        Polynomial<mpz_class> p1(7);
         p1.addNode(3, 6);
         p1.addNode(2, 2);
         p1.addNode(1, 1);
         p1.addNode(5, 0);
 
-        Polynomial<int> p2(7);
+        Polynomial<mpz_class> p2(7);
         p2.addNode(6, 4);
         p2.addNode(1, 3);
         p2.addNode(2, 1);
@@ -371,7 +372,7 @@ TEST_CASE("Testing division, gcd") {
 
         auto gcd = p1.gcd(p2);
 
-        Polynomial<int> expected(7);
+        Polynomial<mpz_class> expected(7);
         expected.addNode(1, 0);
 
         REQUIRE(gcd == expected);
