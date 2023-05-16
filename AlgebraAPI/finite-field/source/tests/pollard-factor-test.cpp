3aaf5f8c6acbcc7e4f4dfbe2ff86c70882c2b1b6
hoksly <realhoksly@gmail.com>
2023-05-16 23:22:14 +0300

    stable tests

--- AlgebraAPI/finite-field/source/tests/pollard-factor-test.cpp
+++ AlgebraAPI/finite-field/source/tests/pollard-factor-test.cpp
@@ -1,11 +1,13 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <gmpxx.h>
+#include <time.h>
+
+#include <chrono>
+#include <random>
+
 #include "../../../doctest.h"
 #include "../../mod-math.h"
 #include "utils.h"
-#include <random>
-#include <time.h>
-#include <chrono>
-#include <gmpxx.h>
 
 using namespace modular;
 
@@ -15,36 +17,42 @@ TEST_CASE("Testing Pollard factorization") {
 
     SUBCASE("Test1") {
         //  T mod = 8*1763668414462081;
-    //     std::chrono::time_point<std::chrono::system_clock> startTime, endTime;
+        //     std::chrono::time_point<std::chrono::system_clock> startTime, endTime;
 
-    //     startTime = std::chrono::system_clock::now();
-    //     std::vector<modNum<T>> nf0 = naiveFactorize(modNum<T>(7*1763668414462081, mod));
-    //     endTime = std::chrono::system_clock::now();
-    //     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count() << " ms" << std::endl;
+        //     startTime = std::chrono::system_clock::now();
+        //     std::vector<modNum<T>> nf0 = naiveFactorize(modNum<T>(7*1763668414462081, mod));
+        //     endTime = std::chrono::system_clock::now();
+        //     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(endTime -
+        //     startTime).count() << " ms" << std::endl;
 
-    //     startTime = std::chrono::system_clock::now();
-    //     std::vector<modNum<T>> pf0 = factorize(modNum<T>(7*1763668414462081, mod));
-    //     endTime = std::chrono::system_clock::now();
-    //     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count() << " ms" << std::endl;
-    // }
+        //     startTime = std::chrono::system_clock::now();
+        //     std::vector<modNum<T>> pf0 = factorize(modNum<T>(7*1763668414462081, mod));
+        //     endTime = std::chrono::system_clock::now();
+        //     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(endTime -
+        //     startTime).count() << " ms" << std::endl;
+        // }
         mpz_class numb1 = 46181;
         mpz_class numb2 = 31393;
         mpz_class numb = numb1 * numb2;
         mpz_class mod2 = numb * 2;
-
 
         std::chrono::time_point<std::chrono::system_clock> startTime, endTime;
 
         startTime = std::chrono::system_clock::now();
         std::vector<modNum<T2>> nf10 = naiveFactorize(modNum<T2>(numb, mod2));
         endTime = std::chrono::system_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count() << " ms" << std::endl;
+        std::cout
+            << std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count()
+            << " ms" << std::endl;
 
         startTime = std::chrono::system_clock::now();
         std::vector<modNum<T2>> pf10 = factorize(modNum<T2>(numb, mod2));
         endTime = std::chrono::system_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count() << " ms" << std::endl;
-
+        std::cout
+            << std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count()
+            << " ms" << std::endl;
+        sort(nf10.begin(), nf10.end());
+        sort(pf10.begin(), pf10.end());
         REQUIRE(nf10 == pf10);
 
         T mod = 4853343968;
@@ -67,10 +75,11 @@ TEST_CASE("Testing Pollard factorization") {
         std::vector<modNum<T>> factors3{modNum<T>(433, mod)};
         REQUIRE(pf3 == factors3);
 
-        std::vector<modNum<T>> pf4 = factorize(modNum<T>(1232, mod+1));
+        std::vector<modNum<T>> pf4 = factorize(modNum<T>(1232, mod + 1));
         sort(pf4.begin(), pf4.end());
-        std::vector<modNum<T>> factors4{modNum<T>(2, mod+1), modNum<T>(2, mod+1), modNum<T>(2, mod+1),
-                                        modNum<T>(2, mod+1), modNum<T>(7, mod+1), modNum<T>(11, mod+1)};
+        std::vector<modNum<T>> factors4{modNum<T>(2, mod + 1), modNum<T>(2, mod + 1),
+                                        modNum<T>(2, mod + 1), modNum<T>(2, mod + 1),
+                                        modNum<T>(7, mod + 1), modNum<T>(11, mod + 1)};
         REQUIRE(pf4 == factors4);
     }
 
@@ -78,7 +87,7 @@ TEST_CASE("Testing Pollard factorization") {
         for (long long i = 2; i <= 10000; ++i) {
             bool isExceptionThrown = false;
             try {
-                std::vector<modNum<T>> num1 = factorize(modNum<T>(i, i+1));
+                std::vector<modNum<T>> num1 = factorize(modNum<T>(i, i + 1));
 
             } catch (std::exception &e) {
                 isExceptionThrown = true;
@@ -93,7 +102,7 @@ TEST_CASE("Testing Pollard factorization") {
 
         std::vector<modNum<T>> f1 = factorize(a);
 
-        modNum<T> mult (1,mod);
+        modNum<T> mult(1, mod);
         bool isExceptionThrown = false;
         for (auto numb : f1) {
             if (!isPrimeSimple(numb.getValue()))
